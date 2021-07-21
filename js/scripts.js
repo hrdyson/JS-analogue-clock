e9ec50652795382885762796ec04987929bd47d1
hrdyson <hannahdyson@talktalk.net>
2021-07-21 20:08:51 +0100

    Moving select elements into its own function

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 let objClock = {
     init:function(){
+        this.selectingElements();
+        console.log(this);
+    },
+    selectingElements:function(){
         this.objHourHand = document.querySelector('.hourHand');
         this.objMinuteHand = document.querySelector('.minuteHand');
         this.objSecondHand = document.querySelector('.secondHand');
-        console.log(this);
-    }
+    }   
 }
 
 objClock.init();
