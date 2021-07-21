f1f08741f783cde35d272c6544c0c5afc61fd847
hrdyson <hannahdyson@talktalk.net>
2021-07-21 20:01:39 +0100

    Creating JS object and selecting required elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('hello');
+let objClock = {
+    init:function(){
+        this.objHourHand = document.querySelector('.hourHand');
+        this.objMinuteHand = document.querySelector('.minuteHand');
+        this.objSecondHand = document.querySelector('.secondHand');
+        console.log(this);
+    }
+}
+
+objClock.init();
