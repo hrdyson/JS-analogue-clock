d6c591534671b4caa3d2ebd55607efac6444ef86
hrdyson <hannahdyson@talktalk.net>
2021-07-21 20:58:29 +0100

    Adding calculation for hours angle and output

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,9 @@ let objClock = {
     getCurrentTime:function(){
         let now = new Date();
         this.hours = now.getHours();
+        if(hours >= 12){
+            this.hours = this.hours - 12;
+        }
         this.minutes = now.getMinutes(); 
         this.seconds = now.getSeconds();  
     },
@@ -30,9 +33,18 @@ let objClock = {
         let angle = percentage * 360;
         return angle;
     },
+    calculateHourAngle:function(){
+        let currentSeconds = this.hours * 3600;
+        currentSeconds = currentSeconds + (this.minutes * 60);
+        currentSeconds = currentSeconds + this.seconds;
+        let percentage = currentSeconds / 43200;
+        let angle = percentage * 360;
+        return angle;
+    },
     adjustHands:function(){
         this.objSecondHand.style.transform = 'rotate('+this.secondsAngle+'deg)';
         this.objMinuteHand.style.transform = 'rotate('+this.minutesAngle+'deg)';
+        this.objHourHand.style.transform = 'rotate('+this.hoursAngle+'deg)';
     }
 }
 
